4652d45ef6a0a9314c5ec47a39c8d649eb851b76
Louis-Philip Marcoux <louisphilipmarcoux@gmail.com>
2025-11-14 12:47:31 -0500

    change env variables import

--- postgres/init.sh
+++ postgres/init.sh
@@ -5,8 +5,8 @@ set -e
 # We connect to the primary DB to run our SQL command.
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
     -- Create the new database for MLflow
-    CREATE DATABASE $MLFLOW_DB;
+    CREATE DATABASE ${MLFLOW_DB};
     
     -- Grant all privileges on the new database to our user
-    GRANT ALL PRIVILEGES ON DATABASE $MLFLOW_DB TO $POSTGRES_USER;
+    GRANT ALL PRIVILEGES ON DATABASE ${MLFLOW_DB} TO ${POSTGRES_USER};
 EOSQL
